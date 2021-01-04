1361cbf17ff69ac2a7191289d4e3d2bc94b83ff1
Raj Shah <raj21101998@gmail.com>
2021-01-04 14:00:40 +0530

    adding src file and data

---
+++ src/ETL.py
@@ -0,0 +1,46 @@
+class BankLoan:
+
+    def __init__(self, input_path=None):
+        self.input_path = input_path
+        self.spark_session = (SparkSession.builder
+                              .master("local[1]")
+                              .appName("Bank_Loan")
+                              .getOrCreate())
+
+    # Read CSV file from source location
+    def extract(self):
+        return clean(spark.read.options(header="true", inferSchema="true", delimiter=",").csv(self.input_path))
+
+    # Select Columns with transformations
+    def transform(self, df):
+        return df
+
+    # Save the final Dataframe in your desired location in parquet/json format
+    def load(self, transformedDF):
+        transformedDF.write\
+            .bucketBy(4,"Country")\
+            .saveAsTable("country_wise_loans", format="parquet")
+        # return transformedDF.show()
+        
+    # Pipelining previous functions
+    def run(self):
+        self.load(self.transform(self.extract()))
+
+
+if __name__ == "__main__":
+    import argparse
+    from pyspark.sql import SparkSession
+    from pyspark.context import SparkContext
+    from data_clean import clean
+
+    # Used to make it work with python directly without involving spaprk-submit
+    sc = SparkContext('local')
+    spark = SparkSession(sc)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_path',
+                        type=str,
+                        default='data/IBRD_Statement_Of_Loans_-_Historical_Data.csv')
+    args = parser.parse_args()
+    bk = BankLoan(input_path=args.input_path)
+    bk.run()
