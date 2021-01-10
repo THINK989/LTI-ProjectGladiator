87fd2bd748f09ce82d85c1ea406c10a5ba613d1b
flx0101 <codevelop0101@gmail.com>
2021-01-10 15:39:54 +0530

    country data

--- src/data_clean.py
+++ src/data_clean.py
@@ -2,7 +2,7 @@ import pyspark.sql.functions as F
 from pprint import pprint 
 from collections import defaultdict
 import datetime
-
+import csv
 def delete_columns(df):
     count_ = df.count()
     dict_ = {column:df.filter(df[column].isNull()).count() for column in df.columns}
@@ -14,23 +14,35 @@ def type_dict(df):
     type_dict = defaultdict(list)
     for col_name,type in df.dtypes:
         type_dict[type].append(col_name)
+    print(type_dict)
     
     for key, val in type_dict.items():
         if key in ["double","float"]:
             df = df.fillna(0,subset=val)    
         elif key == "string":
             df = df.fillna("Others", subset=["project_name","region","country"])
-        elif key == "date":
-            df = df.fillna(F.to_date(F.lit('1850/01/01'),'yyyy/MM/dd'), subset=["effective_date","closed_date","last_disbursed_date"])\
-                    .withColumn("Days_to_Sign_the_loan",F.when(F.col("agreement_signing_date").isNull() | F.col("board_approval_date").isNull() ,\
-                       F.lit(float("inf"))).otherwise(F.datediff("agreement_signing_date","board_approval_date")))\
-                    .withColumn("Time_taken_for_Repayment",F.when(F.col("last_repayment_date").isNull() | F.col("last_repayment_date").isNull() ,\
-                       F.lit(float("inf"))).otherwise(F.datediff("last_repayment_date","first_repayment_date")))\
-                           .drop("last_repayment_date","first_repayment_date","agreement_signing_date","board_approval_date")
+        # elif key == "date":
+        #     df = df.fillna(F.to_date(F.lit('1850/01/01'),'yyyy/MM/dd'), subset=["effective_date","closed_date","last_disbursed_date"])\
+        #             .withColumn("Days_to_Sign_the_loan",F.when(F.col("agreement_signing_date").isNull() | F.col("board_approval_date").isNull() ,\
+        #                F.lit(float("inf"))).otherwise(F.datediff("agreement_signing_date","board_approval_date")))\
+        #             .withColumn("Time_taken_for_Repayment",F.when(F.col("last_repayment_date").isNull() | F.col("last_repayment_date").isNull() ,\
+        #                F.lit(float("inf"))).otherwise(F.datediff("last_repayment_date","first_repayment_date")))\
+        #                    .drop("last_repayment_date","first_repayment_date","agreement_signing_date","board_approval_date")
             
+    countryCode=df.select('country_code','country').rdd.collect()
+    # country=df.select('country').rdd.flatMap(lambda x: x).collect()
+    countryList=list(set(list(countryCode)))
+    # for i in countryList:
+    #     print(i['country_code'],i['country'])
+    # with open('country.csv', 'w', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow(["country_code", "country"])
+    #     for i in countryList:
+    #         writer.writerow([str(i['country_code']),str(i['country'])])
+    
     return df
 
     
 
 def preprocess(df):
-    return type_dict(delete_columns(df))
+    return type_dict(df)
